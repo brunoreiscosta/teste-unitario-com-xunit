9851c93224232e8971b67488a01dbf083ba23869
Bruno Costa <reis_bruno94@outlook.com>
2022-05-13 22:05:19 -0300

    Adicionado novos métodos de teste para as outras operações aritiméticas

--- calculos.tests/CalculadoraTest.cs
+++ calculos.tests/CalculadoraTest.cs
@@ -17,5 +17,45 @@ namespace calculos.tests
             //Verifica se o resultado é igual a 30        
             Assert.Equal(30, resultado);
         }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void RestoDivisao_DeveRetornarZero(int value)
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.RestoDivisao(12, value);
+            //Verifica se o resto da divisão é 0
+            Assert.Equal(0, resultado.resto);
+        }
+
+        [Fact]
+        public void Subtracao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Subtracao(20, 10);
+            //Verifica se o resultado é igual a 10
+            Assert.Equal(10, resultado);
+        }
+
+        [Fact]
+        public void Divisao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Divisao(100, 10);
+            //Verifica se o resultado é igual a 10
+            Assert.Equal(10, resultado);
+        }
+
+        [Fact]
+        public void Multiplicao_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Multiplicao(5, 2);
+            //Verifica se o resultado é igual a 10
+            Assert.Equal(10, resultado);
+        }
     }
 }
