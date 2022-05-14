f59aea76cf4d0a8f4f9ec6066bb4d0315eaa6b67
Bruno Costa <reis_bruno94@outlook.com>
2022-05-13 21:42:32 -0300

    Adicionar arquivos de projeto.

---
+++ calculos.tests/CalculadoraTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace calculos.tests
+{
+    public class CalculadoraTest
+    {
+        [Fact]
+        public void Soma_DeveRetornarOValorCorreto()
+        {
+            Calculadora c = new Calculadora();
+            var resultado = c.Soma(10, 20);
+            //Verifica se o resultado é igual a 30        
+            Assert.Equal(30, resultado);
+        }
+    }
+}
